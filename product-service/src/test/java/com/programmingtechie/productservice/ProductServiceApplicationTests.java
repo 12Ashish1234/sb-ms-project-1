5c9d0003df9d04fd0847fe9f2db9608618a1b803
Ashish <ashishd52280@gmail.com>
2023-08-22 00:03:49 +0530

    Integration testcases added

--- product-service/src/test/java/com/programmingtechie/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/programmingtechie/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,73 @@
 package com.programmingtechie.productservice;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.assertions.Assertions;
+import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.repository.ProductRepository;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	/*
+	 * This step is for creating a MongoDB Test Container using docker. Static
+	 * keyword is used to statically access the container at all times
+	 */
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
+	/*
+	 * This class creates mock request in Test container to check if data will be
+	 * added to the MongoDB container or not. The sample data is in
+	 * getProductRequest() method.
+	 */
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product").contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString)).andExpect(status().isCreated());
+
+		// This is just to confirm that the number of records is 1 or not
+		Assertions.assertTrue(productRepository.findAll().size() == 1);
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder().name("iPhone 13").description("iphone 13").price(BigDecimal.valueOf(1200))
+				.build();
 	}
 
 }
